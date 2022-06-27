a2a549905239cbd366224c5d8cebfb9019007ef7
KwakYV <kvakyur@gmail.com>
2022-06-27 18:05:38 +0300

    Init commit

---
+++ src/main/java/ru/wpz/WpzApplication.java
@@ -0,0 +1,13 @@
+package ru.wpz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/wpz/WpzApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wpz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
