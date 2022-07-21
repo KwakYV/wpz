3570826611bf195d604c3357beabba6a4428164c
Evgeny Khavdey <khavdey61@yandex.ru>
2022-07-21 14:23:33 +0300

    Добавлено получение отчета в указанный день по организациям

--- src/main/java/ru/wpz/controller/ReportController.java
+++ src/main/java/ru/wpz/controller/ReportController.java
@@ -10,6 +10,7 @@ import ru.wpz.dto.ReportPeriodDto;
 import ru.wpz.service.ReportService;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @RestController
@@ -22,22 +23,20 @@ public class ReportController {
 
     @GetMapping("/organization/{id}")
     @ApiOperation("Получение отчетов занятых парковочных мест по организациям в данный момент")
-    public ReportMomentDto showDevOrg(@PathVariable long id){
+    public ReportMomentDto showDevOrg(@PathVariable long id) {
         return reportService.findDevOrg(id);
     }
 
 
     @PostMapping("/organization/day")
     @ApiOperation("Получение отчетов занятых парковочных мест по организациям в указанный день")
-    public DayReportDto dayDevOrg(@RequestBody long id, @RequestBody LocalDateTime day){
+    public List<DayReportDto> dayDevOrg(@RequestBody long id, @RequestBody LocalDateTime day) {
         return reportService.findDayDevOrg(id, day);
     }
 
     @PostMapping("/organization/period")
     @ApiOperation("Получение отчетов занятых парковочных мест по организациям в указанный период времени")
-    public ReportPeriodDto periodDevOrg(@RequestBody long id, @RequestBody LocalDateTime start, @RequestBody LocalDateTime end){
+    public ReportPeriodDto periodDevOrg(@RequestBody long id, @RequestBody LocalDateTime start, @RequestBody LocalDateTime end) {
         return reportService.findPeriod(id, start, end);
     }
-
-
 }
--- src/main/java/ru/wpz/service/ReportService.java
+++ src/main/java/ru/wpz/service/ReportService.java
@@ -30,52 +30,57 @@ public class ReportService {
 
     public void createReport(Device device, Message message) {
         Report report = new Report();
-            report.setTimeMessage(message.getCreatedDt());
-            report.setStatus(message.getStatus());
-            report.setDevice(device);
-            report.setZoneId(device.getZoneId());
-            report.setBuilding(device.getZoneId().getBuilding());
-            report.setOrganizationId(device.getZoneId().getBuilding().getOrganization());
-            reportRepository.save(report);
+        report.setTimeMessage(message.getCreatedDt());
+        report.setStatus(message.getStatus());
+        report.setDevice(device);
+        report.setZoneId(device.getZoneId());
+        report.setBuilding(device.getZoneId().getBuilding());
+        report.setOrganizationId(device.getZoneId().getBuilding().getOrganization());
+        reportRepository.save(report);
     }
 
     public ReportMomentDto findDevOrg(long id) {
         Organization organization = organizationRepository.findById(id).orElse(null);
-        List<Device> deviceList = null;
-        if (organization != null){
-            deviceList = getAllDevice(organization);
-        }
+        List<Device> deviceList = getAllDevice(organization);
         return createReportMoment(deviceList);
     }
 
-    public DayReportDto findDayDevOrg(long id, LocalDateTime day) {
-        List<Report> reportList = null;
+    public List<DayReportDto> findDayDevOrg(long id, LocalDateTime day) {
         LocalDateTime start = day.with(ChronoField.NANO_OF_DAY, LocalTime.MIN.toNanoOfDay());
         LocalDateTime end = day.with(ChronoField.NANO_OF_DAY, LocalTime.MAX.toNanoOfDay());
         Organization organization = organizationRepository.findById(id).orElse(null);
-        reportList = getReportOnOrg(organization, start, end);
-
-        return null; // пока null
+        List<Report> reportList = getReportOnOrg(organization, start, end);
+        List<Device> deviceList = getAllDevice(organization);
+        List<DayReportDto> list = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            DayReportDto dayReportDto = new DayReportDto();
+            dayReportDto.setNumber(i);
+            dayReportDto.setNumber(deviceList.size());
+            dayReportDto.setPercent(findReportPeriod(deviceList, reportList, start, start.plusHours(1L)).getPercent());
+        }
+        return list;
     }
 
-    public ReportPeriodDto findPeriod(long id,LocalDateTime start, LocalDateTime end){
-        List<Report> reportList = null;
+    public ReportPeriodDto findPeriod(long id, LocalDateTime start, LocalDateTime end) {
         Organization organization = organizationRepository.findById(id).orElse(null);
-        reportList = getReportOnOrg(organization, start, end);
-        return findReportPeriod(reportList, start, end);
+        List<Report> reportList = getReportOnOrg(organization, start, end);
+        List<Device> deviceList = getAllDevice(organization);
+        return findReportPeriod(deviceList, reportList, start, end);
     }
 
-    private List<Device> getAllDevice(Organization org){
+    private List<Device> getAllDevice(Organization org) {
         List<Device> deviceList = new ArrayList<>();
-        for (Building building : org.getBuildings()) {
-            for (Parking parking: building.getParking()) {
-                deviceList.addAll(find(parking.getZoneNumber()));
+        if (org != null) {
+            for (Building building : org.getBuildings()) {
+                for (Parking parking : building.getParking()) {
+                    deviceList.addAll(find(parking.getZoneNumber()));
+                }
             }
         }
         return deviceList;
     }
 
-    private ReportMomentDto createReportMoment(List<Device> list){
+    private ReportMomentDto createReportMoment(List<Device> list) {
         ReportMomentDto reportMomentDto = new ReportMomentDto();
         reportMomentDto.setTotal(list.size());
         reportMomentDto.setStatusTaken((int) list.stream().filter(device -> device.getMessages().get(0).getStatus() == 1).count());
@@ -84,20 +89,23 @@ public class ReportService {
         return reportMomentDto;
     }
 
-    private List<Device> find(int zoneId){
+    private List<Device> find(int zoneId) {
         return deviceRepository.findAll(zoneId).stream()
-                .map(device -> {device.getMessages().subList(0,device.getMessages().size() - 1).clear(); return device;})
+                .map(device -> {
+                    device.getMessages().subList(0, device.getMessages().size() - 1).clear();
+                    return device;
+                })
                 .collect(Collectors.toList());
     }
 
-    private List<Report> getReportOnOrg(Organization organization, LocalDateTime start, LocalDateTime end){
-        if(organization != null){
+    private List<Report> getReportOnOrg(Organization organization, LocalDateTime start, LocalDateTime end) {
+        if (organization != null) {
             return reportRepository.getReport(organization, start, end);
         }
         return null;
     }
 
-    private ReportPeriodDto findReportPeriod(List<Report> listR, LocalDateTime start, LocalDateTime end){
+    private ReportPeriodDto findReportPeriod(List<Device> listD, List<Report> listR, LocalDateTime start, LocalDateTime end) {
         boolean flag = false;
         List<Integer> countingTime = new ArrayList<>();
         LocalDateTime intermediateTime = null;
@@ -105,19 +113,19 @@ public class ReportService {
         Map<Device, List<Report>> result = new HashMap<>();
         for (Report r : listR) {
             Device d = r.getDevice();
-            result.compute(d,(k,v) -> (v == null) ? new ArrayList<>(Arrays.asList(r)): plus(v, r));
+            result.compute(d, (k, v) -> (v == null) ? new ArrayList<>(Arrays.asList(r)) : plus(v, r));
         }
-        for (Map.Entry<Device, List<Report>> entry : result.entrySet()){
+        for (Map.Entry<Device, List<Report>> entry : result.entrySet()) {
             flag = true;
-            for (Report r: entry.getValue()) {
-                if(flag){
+            for (Report r : entry.getValue()) {
+                if (flag) {
                     flag = false;
-                    if (r.getStatus() == 0){
+                    if (r.getStatus() == 0) {
                         minute = start.getMinute() + r.getTimeMessage().getMinute();
                         continue;
                     }
                 }
-                if (r.getStatus() == 1){
+                if (r.getStatus() == 1) {
                     intermediateTime = r.getTimeMessage();
                 } else {
                     minute += intermediateTime.getMinute() + r.getTimeMessage().getMinute();
@@ -125,22 +133,19 @@ public class ReportService {
                 }
                 countingTime.add(minute);
             }
-            if (start != null){
+            if (start != null) {
                 minute += end.getMinute() - intermediateTime.getMinute();
                 intermediateTime = null;
             }
         }
         ReportPeriodDto reportPeriodDto = new ReportPeriodDto();
-        reportPeriodDto.setTotal(countingTime.size());
-        reportPeriodDto.setPercent((double) Math.floorDiv(reportPeriodDto.getTotal() * 60, minute));
+        reportPeriodDto.setTotal(listD.size());
+        reportPeriodDto.setPercent((double) Math.floorDiv(reportPeriodDto.getTotal() * 60 * 100, minute));
         return reportPeriodDto;
     }
 
-    private List<Report> plus(List<Report> list, Report report){
+    private List<Report> plus(List<Report> list, Report report) {
         list.add(report);
         return list;
     }
-
-
-
 }
