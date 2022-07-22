89e1a78a99e1a383e344bcff5e9e04ab55fdba11
EvgenyKhavdey <77062461+EvgenyKhavdey@users.noreply.github.com>
2022-07-22 16:09:10 +0300

    Добавлен Web netty (#13)
    * Добавлен Web netty
    
    * Добавлено получение отчета в данный момент по организациям
    
    * Добавлено получение отчета в указанный период времени по организациям
    
    * Добавлено получение отчета в указанный день по организациям
    
    * Исправление замечаний

--- src/main/java/ru/wpz/service/MessageService.java
+++ src/main/java/ru/wpz/service/MessageService.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.wpz.dto.MessageDto;
+import ru.wpz.entity.Device;
 import ru.wpz.entity.Message;
 import ru.wpz.mapper.MessageMapper;
-import ru.wpz.nio_server.NettyNetwork;
+import ru.wpz.repository.DeviceRepository;
 import ru.wpz.repository.MessageRepository;
 
 import java.util.List;
@@ -18,7 +19,8 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
     private final MessageMapper messageMapper;
-    private final NettyNetwork nettyNetwork;
+    private final DeviceRepository deviceRepository;
+    private final ReportService reportService;
 
     public List<Message> findAll(long devId) {
         return messageRepository.findAll(devId);
@@ -29,8 +31,8 @@ public class MessageService {
     }
 
     @Transactional
-    public void save(Message message) {
-        messageRepository.save(message);
+    public Message save(Message message) {
+       return messageRepository.save(message);
     }
 
     public void delete(Long id) {
@@ -38,10 +40,11 @@ public class MessageService {
     }
 
     public void saveFromKafka(MessageDto messageDto) {
-        Message message = messageMapper.mapMessage(messageDto);
-        save(message);
-        messageDto = messageMapper.mapMessageDto(messageRepository.getMessageBy(message));
-        nettyNetwork.writeMessage(messageDto);
+        Message message = save(messageMapper.mapMessage(messageDto));
+        Device device = deviceRepository.findById(message.getDevId().getId()).orElse(null);
+        if (device != null){
+            reportService.createReport(device, message);
+        }
     }
 }
 
