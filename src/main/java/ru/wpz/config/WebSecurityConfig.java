95e022f3c5d083cd36f12c25c98e7baa17bff6fb
EvgenyKhavdey <77062461+EvgenyKhavdey@users.noreply.github.com>
2022-08-12 10:28:34 +0300

    Lesson7 es khavdey (#22)
    * Lesson6 kwak yv (#20)
    
    * Reimplemented logic of reports, also reimplemented usage of jpa repository
    
    * Added full info for device message which will be stored in report
    
    * Авторизация
    
    Co-authored-by: KwakYV <kvakyur@gmail.com>

---
+++ src/main/java/ru/wpz/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package ru.wpz.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
+        return httpSecurity
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                                )
+                )
+                .accessDeniedHandler(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+                                )
+                )
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authenticationManager(authenticationManager)
+                .securityContextRepository(securityContextRepository)
+                .authorizeExchange()
+                .pathMatchers("/", "/login").permitAll()
+                .pathMatchers("/organization", "/building", "/parking", "/device", "/message", "/report").hasAnyRole("ADMIN")
+                .anyExchange().authenticated()
+                .and()
+                .build();
+    }
+}
